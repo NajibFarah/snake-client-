336af001c9abbf7d973c6aaa606a68c89c2ebb79
Najib <muhammed.najib.farah@gmail.com>
2021-12-02 01:13:45 +0000

    changes

--- play.js
+++ play.js
@@ -1,19 +1,21 @@
-const net = require("net");
+const {connect} = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  
-
-  return conn;
+const handleUserInput = function (key) {
+  if (key = '\u0003')
+    process.exit();
+  console.log();
 };
 
 console.log("Connecting ...");
 connect();
+
+setupInput()
